54533c4bf378f3c6948ffd3d23b8ad34855b829f
CornSeller <166633175+CornSeller69@users.noreply.github.com>
2025-02-20 00:39:11 +0100

    Final prototype commit
    I finally fuckin' made it!

--- assets/pasje.js
+++ assets/pasje.js
@@ -4,9 +4,23 @@ const t_load = new Audio('assets/pipboy.mp3');
 const t_keypress = new Audio('assets/keypress.mp3');
 const t_ost = new Audio('assets/miside.mp3');
 t_ost.loop = true;
+const t_melody = new Audio('assets/melody.mp3');
+const mel1 = new Audio('assets/note1.mp3');
+const mel2 = new Audio('assets/note2.mp3');
+const mel3 = new Audio('assets/note3.mp3');
 
 let ccv = "";
 let cyph_stage = 0;
+
+let mel_progress = '0%';
+let posalt = 1;
+let mel_sequence = {
+     1: 0,
+     2: 0,
+     3: 0,
+     4: 0,
+     5: 0
+};
 
 // losowo generowane hasło z każdą grą:
 let mainPass = Math.floor(10000 + Math.random() * 90000);
@@ -51,7 +65,8 @@ function tryLogin() {
           document.getElementById("haslo").value = "";
           passDebug = 4;
      } else {
-          if (document.getElementById("login").value == "temp3" && document.getElementById("haslo").value == mainPass) {
+          if (document.getElementById("haslo").value == 'thedoor') {correctMelody();
+          } else if (document.getElementById("login").value == "temp3" && document.getElementById("haslo").value == mainPass) {
                t_yes.currentTime = 0;
                t_yes.play();
                document.getElementById("gra3").style.visibility = 'visible';
@@ -176,4 +191,95 @@ function close_gra2() {
      t_load.play();
      setTimeout(function() {t_load.pause(); t_load.currentTime = 0;}, 2500);
      setTimeout(function() {document.getElementById("gra2").style.visibility = 'hidden'; document.getElementById("okienko-gra2").style.visibility = 'hidden';}, 990);
+}
+
+function playMelody() {
+     t_melody.currentTime = 0;
+     t_melody.play();
+}
+function melodyButton(b) {
+    let currentSequence = mel_sequence[1].toString() + mel_sequence[2].toString() + mel_sequence[3].toString() + mel_sequence[4].toString() + mel_sequence[5].toString();
+    console.info(currentSequence);
+    // to co wyżej to poprawka by działało bo z jakiegoś powodu nie chce nwm
+
+    if (posalt < 6) {
+     switch(b) {
+          case 1:
+               mel1.currentTime = 0;
+               mel1.play();
+               mel_sequence[posalt] = 1;
+               posalt++;
+               document.getElementById("mel-bar").style.width = (posalt - 1) * 20 + '%';;
+               break;
+          case 2:
+               mel2.currentTime = 0;
+               mel2.play();
+               mel_sequence[posalt] = 2;
+               posalt++;
+               document.getElementById("mel-bar").style.width = (posalt - 1) * 20 + '%';;
+               break;
+          case 3:
+               mel3.currentTime = 0;
+               mel3.play();
+               mel_sequence[posalt] = 3;
+               posalt++;
+               document.getElementById("mel-bar").style.width = (posalt - 1) * 20 + '%';;
+               break;
+     }
+} else if (posalt >= 5 && currentSequence == '12312') {
+          t_yes.currentTime = 0; t_yes.play();
+          correctMelody();
+     } else if (posalt >= 5 && currentSequence != '12312') {
+          document.getElementById("mel-bar").style.width = '0%';
+          t_error.currentTime = 0; t_error.play();
+          mel_sequence = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0};
+          posalt = 1;
+     }
+}
+
+function correctMelody() {
+     document.getElementById("gra3").style.visibility = 'hidden';
+     document.getElementById("gra4").style.visibility = 'visible';
+}
+
+function submitSurvey() {
+     let a =document.getElementById("r12");
+     let b = document.getElementById("r22");
+     let c = document.getElementById("r31");
+     let d = document.getElementById("r32");
+     let e = document.getElementById("r33");
+     if (a.checked && b.checked && c.checked || d.checked || e.checked) {
+          t_yes.currentTime = 0;
+          t_yes.play();
+          document.getElementById("gra4").style.visibility = 'hidden';
+          document.getElementById("gra5").style.visibility = 'visible';
+     } else {
+          t_error.currentTime = 0;
+          t_error.play();
+     }
+}
+
+function chosenChess(c) {
+     switch(c) {
+          case 1:
+               t_error.currentTime = 0; t_error.play();
+               break;
+          case 2:
+               t_error.currentTime = 0; t_error.play();
+               break;
+          case 3:
+               t_yes.currentTime = 0; t_yes.play();
+               showEnding();
+               document.getElementById("gra5").style.visibility = 'hidden';
+               break;
+     }
+}
+
+function showEnding() {
+     document.getElementById("gra6").style.visibility = 'visible';
+     t_load.currentTime = 0;
+     t_load.play();
+     let a = document.getElementById("hat");
+     a.style.right = 0;
+     setTimeout(function() {a.style.left = 0;}, 10000);
 }
